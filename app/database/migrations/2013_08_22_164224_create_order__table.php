3b3a6b97b8cc6e4710ff482f56c2d94d0bd967d1
George Barba <george@agenaastro.com>
2013-08-23 13:45:35 -0700

    Updated Migrations

--- app/database/migrations/2013_08_22_164224_create_order__table.php
+++ app/database/migrations/2013_08_22_164224_create_order__table.php
@@ -17,6 +17,7 @@ class CreateOrderTable extends Migration {
 			$table->increments('id');
 			$table->string('shipworks_order_id')->unique();
 			$table->date('order_date');
+			$table->foreign('customer_id')->references('id')->on('customers');
 			$table->string('ship_name');
 			$table->string('ship_address1');
 			$table->string('ship_address2');
